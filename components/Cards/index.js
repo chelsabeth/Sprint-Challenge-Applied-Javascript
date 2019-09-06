28a5b714ee3f5c57b8b0b7e5988c3e756698751c
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-06 12:58:07 -0400

    created article component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,38 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios 
+  .get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log('No data was returned', error);
+  });
+
+  
+
+
+  function article(data) {
+      
+    const newArticle = document.createElement('div'),
+    headline = document.createElement('div'),
+    author = document.createElement('div'),
+    imgDiv = document.createElement('div'),
+    img = document.createElement('img'),
+    authorsName = document.createElement('span');
+
+    newArticle.classList('card');
+    headline.classList('headline');
+    author.classList('author');
+    imgDiv.classList('img-container');
+
+    newArticle.appendChild(headline);
+    newArticle.appendChild(author);
+    author.appendChild(imgDiv);
+    imgDiv.appendChild(img);
+    author.appendChild(authorsName);
+
+    return newArticle;
+  }
